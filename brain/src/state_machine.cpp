5187cf4cd819bda125907d0a31e9b144c0fdae9c
Nathaniel Todd-Long <ntoddlong@gmail.com>
2022-07-02 03:37:52 +0000

    Async and realtime examples

--- brain/src/state_machine.cpp
+++ brain/src/state_machine.cpp
@@ -13,6 +13,12 @@ using namespace std;
 
 // use this as base class and implement
 // state machines for each task
+// look into this too:
+// https://github.com/ros2/examples/blob/master/rclcpp/services/async_client/main.cpp
+// also,
+// https://design.ros2.org/articles/realtime_background.html#multithreaded-programming-and-synchronization
+// and,
+// https://orenbell.com/?p=436
 class StateMachine : public rclcpp::Node {
   public:
     StateMachine() : Node("StateMachine") {
